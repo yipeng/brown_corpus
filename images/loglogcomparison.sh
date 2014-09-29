7a57d157431b0f79bf32945984a14288923aaf98
yipeng <yipeng@gmail.com>
2014-09-29 13:40:43 -0400

    Testing

--- images/loglogcomparison.sh
+++ images/loglogcomparison.sh
@@ -11,8 +11,8 @@ set datafile separator ","
 
 set terminal postscript eps color
 set output "loglogcomparison.eps"
-plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
+plot "../word_freq.csv" using 0:2 title "Words", 69971/x title "Zipf"
 
 set terminal pngcairo
 set output "loglogcomparison.png"
-plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
+plot "../word_freq.csv" using 0:2 title "Words", 69971/x title "Zipf"
