213018a467a8773723fa5cff056544b862c3f6d8
yipeng <yipeng@gmail.com>
2014-09-29 12:01:49 -0400

    Testing loglogcomparison

---
+++ images/loglogcomparison.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/gnuplot
+set logscale x
+set logscale y
+set xlabel "Rank"
+set ylabel "Frequency"
+set title "Frequency/Rank plot of Brown Corpus words"
+
+set datafile separator ","
+
+set terminal postscript eps color
+set output "loglogplot.eps"
+plot "../word_freq.csv" using 0:3
+
+set terminal pngcairo
+set output "loglogplot.png"
+plot "../word_freq.csv" using 0:3, 69971/x title "Zipf"
