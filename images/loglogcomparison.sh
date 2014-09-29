ba41523eea9863816fe0866f1ea7cbf088990fb8
yipeng <yipeng@gmail.com>
2014-09-29 13:52:19 -0400

    Something is wrong with my gnuplot, output is not updated

--- images/loglogcomparison.sh
+++ images/loglogcomparison.sh
@@ -8,4 +8,4 @@ set datafile separator ","
 
 set terminal pngcairo
 set output "loglogcomparison.png"
-plot "../word_freq.csv" using 0:3 title "Word", 69971/x title "Zipf"
+plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
