773cee96c5e9578227c13d1fc2be2218a2c1cfb5
yipeng <yipeng@gmail.com>
2014-09-29 12:09:55 -0400

    Bless

--- images/loglogcomparison.sh
+++ images/loglogcomparison.sh
@@ -3,14 +3,14 @@ set logscale x
 set logscale y
 set xlabel "Rank"
 set ylabel "Frequency"
-set title "Frequency/Rank plot of Brown Corpus words"
+set title "Frequency/Rank plot of Brown Corpus"
 
 set datafile separator ","
 
 set terminal postscript eps color
 set output "loglogcomparison.eps"
-plot "../word_freq.csv" using 0:3 title "Word", 69971/x title "Zipf"
+plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
 
 set terminal pngcairo
 set output "loglogcomparison.png"
-plot "../word_freq.csv" using 0:3 title "Word", 69971/x title "Zipf"
+plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
