082927693bd429c9b9f821faf8fd7dc7a9ba40da
yipeng <yipeng@gmail.com>
2014-09-29 12:36:53 -0400

    Back to cairo which looks a lot better

--- images/loglogcomparison.sh
+++ images/loglogcomparison.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/gnuplot
-
 set logscale x
 set logscale y
 set xlabel "Rank"
 set ylabel "Frequency"
 set title "Frequency/Rank plot of Brown Corpus"
+set style line 1 linewidth 3
+set style line 2 linewidth 3
 
 set datafile separator ","
 
@@ -12,6 +13,6 @@ set terminal postscript eps color
 set output "loglogcomparison.eps"
 plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
 
-set terminal png
+set terminal pngcairo
 set output "loglogcomparison.png"
 plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
