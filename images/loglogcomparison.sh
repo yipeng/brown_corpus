6784b2108825e69a0ddd736a90da25299fbbd5e4
yipeng <yipeng@gmail.com>
2014-09-29 13:51:21 -0400

    Test do changes apply?

--- images/loglogcomparison.sh
+++ images/loglogcomparison.sh
@@ -4,15 +4,8 @@ set logscale y
 set xlabel "Rank"
 set ylabel "Frequency"
 set title "Frequency/Rank plot of Brown Corpus"
-set style line 1 linewidth 3
-set style line 2 linewidth 3
-
 set datafile separator ","
-
-set terminal postscript eps color
-set output "loglogcomparison.eps"
-plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
 
 set terminal pngcairo
 set output "loglogcomparison.png"
-plot "../word_freq.csv" using 0:3 title "Words", 69971/x title "Zipf"
+plot "../word_freq.csv" using 0:3 title "Word", 69971/x title "Zipf"
