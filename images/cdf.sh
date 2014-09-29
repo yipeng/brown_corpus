e864e987bc7992277269e434fca517e51789d50d
yipeng <yipeng@gmail.com>
2014-09-29 13:01:33 -0400

    Test CDF

---
+++ images/cdf.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/gnuplot
+set logscale x
+set logscale y
+set xlabel "Rank"
+set ylabel "Frequency"
+set title "CDF of Brown Corpus"
+set style line 1 linewidth 3
+set style line 2 linewidth 3
+
+set datafile separator ","
+
+set terminal pngcairo
+set output "cdf.png"
+a=0
+cummulative_sum(x)=(a=a+x,a)
+plot "../word_freq.csv" using 0:(cummulative_sum($3)) title "Words"
