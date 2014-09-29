5253768d54205fd02d27cd2d19d99bb08a567625
yipeng <yipeng@gmail.com>
2014-09-29 12:08:11 -0400

    Fix images

--- images/loglogcomparison.sh
+++ images/loglogcomparison.sh
@@ -8,9 +8,9 @@ set title "Frequency/Rank plot of Brown Corpus words"
 set datafile separator ","
 
 set terminal postscript eps color
-set output "loglogplot.eps"
-plot "../word_freq.csv" using 0:3
+set output "loglogcomparison.eps"
+plot "../word_freq.csv" using 0:3 title "Word", 69971/x title "Zipf"
 
 set terminal pngcairo
-set output "loglogplot.png"
-plot "../word_freq.csv" using 0:3, 69971/x title "Zipf"
+set output "loglogcomparison.png"
+plot "../word_freq.csv" using 0:3 title "Word", 69971/x title "Zipf"
