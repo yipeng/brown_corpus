4d8ad9bb5ba1a6180d0e619c71bf35e2cbedeeac
yipeng <yipeng@gmail.com>
2014-09-30 09:05:07 -0400

    I've been looking at it all wrong

--- images/logbin.sh
+++ images/logbin.sh
@@ -12,6 +12,9 @@ set output "logbinned.png"
 binwidth=5
 bin(x,width)=width*floor(x/width) + binwidth/2.0
 
-flabel(y)=sprintf("y=%.2f", y)
-plot '../word_freq.csv' using (bin($0, binwidth)):($3):(flabel($3)) with labels point offset character 0,character 1 tc rgb "blue", ($3)/x title "Zipf" smooth freq
+#flabel(y)=sprintf("y=%.2f", y)
+sum=0
+s(x)          = ((sum=sum+1), 0)
+plot '../word_freq.csv' using (bin($0, binwidth)):($3) smooth freq, s(x)/x title "Zipf"
+#:(flabel($3)) with labels point offset character 0,character 1 tc rgb "blue", ($3)/x title "Zipf" smooth freq
 set output
