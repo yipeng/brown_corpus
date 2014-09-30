d414638360384a6ea8ef9084c59f08f35307ac4d
yipeng <yipeng@gmail.com>
2014-09-30 09:28:33 -0400

    :)

--- images/logbin.sh
+++ images/logbin.sh
@@ -13,8 +13,10 @@ binwidth=5
 bin(x,width)=width*floor(x/width) + binwidth/2.0
 
 #flabel(y)=sprintf("y=%.2f", y)
-sum=0
-s(x)          = ((sum=sum+1), 0)
-plot '../word_freq.csv' using (bin($0, binwidth)):($3) smooth freq, s(x)/x title "Zipf"
+#sum = 0
+#s(x) = ((sum=sum+1), 0)
+#plot '../word_freq.csv' using ($0):(s($2))
+
+plot '../word_freq.csv' using (bin($0, binwidth)):($3), 981716/x title "Zipf"
 #:(flabel($3)) with labels point offset character 0,character 1 tc rgb "blue", ($3)/x title "Zipf" smooth freq
 set output
