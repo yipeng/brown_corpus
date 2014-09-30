f0fa52d0db04c1de15f2a5058877fe012862750e
yipeng <yipeng@gmail.com>
2014-09-30 07:27:45 -0400

    With labels points

--- images/logbin.sh
+++ images/logbin.sh
@@ -11,7 +11,9 @@ set terminal pngcairo
 set output "logbinned.png"
 binwidth=5
 bin(x,width)=width*floor(x/width) + binwidth/2.0
-plot '../word_freq.csv' using (bin($0, binwidth)):($3) smooth freq with labels points
+
+flabel(y)=sprintf("y=%.2f", y)
+plot '../word_freq.csv' using (bin($0, binwidth)):($3):(flabel($0)) w labels smooth freq
 #, 69971/x title "Zipf"
 
 set output
