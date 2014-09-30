cd51d1ca3c2368166a01abe9b7288316d4345708
yipeng <yipeng@gmail.com>
2014-09-30 10:04:32 -0400

    Remove zipf

--- images/logbin.sh
+++ images/logbin.sh
@@ -9,7 +9,7 @@ set datafile separator ","
 
 set terminal pngcairo
 set output "logbinned.png"
-binwidth=10
+binwidth=2
 bin(x,width)=width*floor(x/width) + binwidth/2.0
 
 #flabel(y)=sprintf("y=%.2f", y)
@@ -17,6 +17,6 @@ bin(x,width)=width*floor(x/width) + binwidth/2.0
 #s(x) = ((sum=sum+1), 0)
 #plot '../word_freq.csv' using ($0):(s($2))
 
-plot '../word_freq.csv' using (bin($0, binwidth)):($3) smooth frequency, (bin($0, binwidth))/x title "Zipf" smooth frequency
+plot '../word_freq.csv' using (bin($0, binwidth)):($3) smooth frequency
 #:(flabel($3)) with labels point offset character 0,character 1 tc rgb "blue", ($3)/x title "Zipf" smooth freq
 set output
