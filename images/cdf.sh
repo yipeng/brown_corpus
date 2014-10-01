78927ad9e0595daf5857c1e6791ca036c3e29f90
yipeng <yipeng@gmail.com>
2014-10-01 07:37:36 -0400

    cdf test

--- images/cdf.sh
+++ images/cdf.sh
@@ -6,13 +6,13 @@ set ylabel "Frequency"
 set title "Frequency/Rank of Brown Corpus"
 set datafile separator ","
 set xrange[0.1:100000]
+set yrange[0.1:1000000]
 
-set terminal postscript eps color
-set output "logcdf.eps"
-plot "../word_freq.csv" using 0:3, 69971/x title "Zipf"
-set output 
+binwidth=0.35020056481
+bin(x,width)=width*floor(x/width) + binwidth/2.0
+
 
 set terminal pngcairo
 set output "logcdf.png"
-plot "../word_freq.csv" using 0:3 cumul title "Words"
+plot '../word_freq.csv' using (bin($0, binwidth)):($3) smooth cumul title "Words", 69971/x title "Zipf"
 set output
