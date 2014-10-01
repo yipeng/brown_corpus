9e6c2c967c49c9a1b1085fa4a7ebcbadb39a7782
yipeng <yipeng@gmail.com>
2014-10-01 10:14:35 -0400

    Added materialized values for zipf

--- gen_word_freq.py
+++ gen_word_freq.py
@@ -18,3 +18,23 @@ words_vals = list(words.values())
 f = open("word_freq.csv", "w")
 for idx,i in enumerate(words_keys):
 	f.write(str(idx+1)+','+'"'+str(i)+'",'+str(words_vals[idx])+'\n')
+
+f.close()
+
+
+def gen_zipf():
+	x = open("zipf_vals.csv", "w")
+	#f = open("word_freq.csv")
+	with open("word_freq.csv") as f:
+    		content = f.readlines()
+	
+	base = 0			
+	for idx,ct in enumerate(content):
+		tokens = ct.split(',')
+		if (idx == 0):
+			base = tokens[2]
+		x.write(tokens[0]+','+ str(int(base)/int(tokens[0]))+'\n')
+	x.close()
+	f.close()
+
+gen_zipf()
