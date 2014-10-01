2fdb1b78c7f724a2d98181e3bcbaf28b81caa8bf
yipeng <yipeng@gmail.com>
2014-10-01 07:50:30 -0400

    cdf styling zipf line

--- images/cdf.sh
+++ images/cdf.sh
@@ -8,6 +8,9 @@ set datafile separator ","
 set xrange[0.1:50000]
 set yrange[0.1:1000000]
 
+set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 pi -1 ps 1.5
+set pointintervalbox 3
+
 binwidth=0.35020056481
 bin(x,width)=width*floor(x/width) + binwidth/2.0
 
