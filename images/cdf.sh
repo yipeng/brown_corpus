af5095b6be939370db250f4e012f4a85d5a71047
yipeng <yipeng@gmail.com>
2014-10-01 10:33:09 -0400

    again

--- images/cdf.sh
+++ images/cdf.sh
@@ -11,8 +11,8 @@ set yrange[0.1:1000000]
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 pi -1 ps 1.5
 set pointintervalbox 3
 
-binwidth=1174.16412705
-bin(x,width)=floor(x/width) + binwidth/2.0
+binwidth=1174.16412705/2
+bin(x,width)=binwidth*floor(x/width) + binwidth/2.0
 
 
 set terminal pngcairo
