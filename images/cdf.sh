d701ffb71c2cacaafbfad4d0eeb87719e8f43f4d
yipeng <yipeng@gmail.com>
2014-10-01 07:48:54 -0400

    cdf styling zipf line

--- images/cdf.sh
+++ images/cdf.sh
@@ -14,5 +14,5 @@ bin(x,width)=width*floor(x/width) + binwidth/2.0
 
 set terminal pngcairo
 set output "logcdf.png"
-plot '../word_freq.csv' using 0:3 title "Zipf" smooth cumul
+plot '../word_freq.csv' using 0:3 title "Zipf" with linespoints ls 1 smooth cumul
 set output
