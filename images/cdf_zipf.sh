9cb943290c60e819a6aa32c85e13512e3f07c681
yipeng <yipeng@gmail.com>
2014-10-02 10:35:59 -0400

    Updated range

--- images/cdf_zipf.sh
+++ images/cdf_zipf.sh
@@ -6,7 +6,7 @@ set ylabel "Cumulative Frequency"
 set title "Frequency/Rank of Brown Corpus"
 set datafile separator ","
 set xrange[0.1:50000]
-set yrange[0.1:1000000]
+set yrange[0.01:1]
 
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 pi -1 ps 1.5
 set pointintervalbox 3
