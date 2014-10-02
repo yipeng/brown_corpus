84079770c771eb0d2ba8ae88c4147359a6461cb9
yipeng <yipeng@gmail.com>
2014-10-01 20:06:51 -0400

    examine log scale

--- images/cdf2.sh
+++ images/cdf2.sh
@@ -5,7 +5,7 @@ set xlabel "Rank"
 set ylabel "Cumulative Frequency"
 set title "Frequency/Rank of Brown Corpus"
 set datafile separator ","
-set xrange[0.1:50000]
+set xrange[0.1:100000]
 set yrange[0.1:1000000]
 
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 pi -1 ps 1.5
